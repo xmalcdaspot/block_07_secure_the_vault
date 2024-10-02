a8af6cb67122d9f8818d57050ca08002961ddccb
xmalcdaspot <malcolmroliver@gmail.com>
2024-10-01 22:07:17 -0400

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Pseudocode: The sum of the variables will produce the ending result in which will be the combination to the vault.
+//Create a string for the user
+let userOpen = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+//Produce 3 variables with the ending result in which will be the combination to the vault, 10 - 40 - 39
+//the 1st variable will consist of an arithmetic that will generate the number 10
+let firstNumber = (4 + 3) + (9 / 3);
+console.log(firstNumber)
+//the 2nd variable will consist of an arithmetic that will generate the number 40
+let secondNumber = (21 * 2) - (22 / 11);
+console.log(secondNumber)
+//the 3rd variable will consist of an arithmetic that will generate the number 39
+let thirdNumber = (33 - 3) + (3 * 3);
+console.log(thirdNumber)
+//create a popup dialog box that displays the secret combination to the vault
+//alert(`${userOpen} ${firstNumber} - ${secondNumber} - ${thirdNumber}.`);
